8674d2ef02b4a9940534e7c90757506dc12a2b3f
Мира Странная <miraityan2004@gmail.com>
2019-08-12 21:35:43 +0400

    Fix interior vertex condition

--- Source/OBJ_Loader.h
+++ Source/OBJ_Loader.h
@@ -969,7 +969,7 @@ namespace objl
 
 					// If Vertex is not an interior vertex
 					float angle = math::AngleBetweenV3(pPrev.Position - pCur.Position, pNext.Position - pCur.Position) * (180 / 3.14159265359);
-					if (angle <= 0 && angle >= 180)
+					if (angle <= 0 || angle >= 180)
 						continue;
 
 					// If any vertices are within this triangle
